5fb0ece795932a3e4b0944b22a640edaf147c541
AnisSwe <iam0anis@gmail.com>
2026-02-20 16:25:59 +0600

    Model practice"

---
+++ StudentProfile/StudentProfile/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentProfile.Models;
+
+namespace StudentProfile.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Profile(int id)
+        {
+            var students = new List<Student>
+    {
+        new Student { Id = 1, Name = "John Doe", Age = 17, Department = "CS", CGPA = 3.8 },
+        new Student { Id = 2, Name = "Jane Smith", Age = 20, Department = "IT", CGPA = 3.2 },
+        new Student { Id =3,Name = "MUnna", Age = 25 ,Department = "ICE", CGPA = 3.97}
+    };
+
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+    }
+}
