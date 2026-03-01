f849742939ed189640f0004b01b124eba41ef9f5
AnisSwe <iam0anis@gmail.com>
2026-03-02 00:49:21 +0600

    custom routing

---
+++ Course/Course/Controllers/CourseController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Course.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course.Controllers
+{
+    public class CourseController : Controller
+    {
+        private static List<Courses> courses = new List<Courses>
+    {
+        new Courses { Id = 1, Title = "Calculus I", Credit =3, Semester = "Fall" },
+        new Courses { Id = 2, Title = "Physics I", Credit = 4, Semester = "Fall" },
+        new Courses{ Id = 3, Title = "Data Structures", Credit = 3, Semester = "Spring" },
+        new Courses { Id = 4, Title = "Database Systems", Credit = 3, Semester = "Spring" },
+        new Courses { Id = 5, Title = "Algorithms", Credit = 3, Semester = "Summer" }
+    };
+        [Route("Course/List")]
+        [Route("Course/List/{semester?}")]
+        public IActionResult List(string Semester)
+        {
+            if (string.IsNullOrEmpty(Semester))
+            {
+                return View(courses);
+            }
+            else
+            {
+                var filteredCourses = courses
+                    .Where(c => c.Semester.Equals(Semester, System.StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+                return View(filteredCourses);
+            }
+        }
+    }
+}
+
---
+++ Course/Course/Models/Courses.cs
@@ -0,0 +1,10 @@
+﻿namespace Course.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int? Credit { get; set; }
+        public string ? Semester {  get; set; }
+    }
+}
